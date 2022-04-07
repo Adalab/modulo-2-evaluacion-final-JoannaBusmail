174bbc6ab0871f54251270d2b9b94e8b31b00e9b
JoannaBusmail <joanna.busmail@gmail.com>
2022-04-07 21:47:48 +0200

    add html elements into js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+//CONSTANTES DE ELEMENTOS HTML
+
+const form = document.querySelector('.js_form');
+const input = document.querySelector ('.js_input');
+const searchBtn = document.querySelector ('.js_searchBtn');
+const resetBtn = document.querySelector ('.js_resetBtn');
+const favorites = document.querySelector ('.js_favorites');
+const list = document.querySelector ('.js_list');
