9a4a084edd2c7e47d5004d8ab19a623fba616d66
JoannaBusmail <joanna.busmail@gmail.com>
2022-04-07 23:04:18 +0200

    add fetch, searchbtn click and handleclick function

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,32 @@ const searchBtn = document.querySelector ('.js_searchBtn');
 const resetBtn = document.querySelector ('.js_resetBtn');
 const favorites = document.querySelector ('.js_favorites');
 const list = document.querySelector ('.js_list');
+
+let searchList = [];
+
+
+
+function handleClick (event){
+event.preventDefault();
+const inputValue = input.value;
+
+
+fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
+.then((response) => response.json())
+.then((data)=> {console.log(data);
+searchList = data.drinks.map((item)=>{
+    const newSearchList = {
+        name : item.strDrink,
+        image: item.strImageSource,
+      id:item.idDrink,
+    };
+    return newSearchList;
+});
+console.log(searchList);
+});
+
+}
+
+
+//LISTENERS
+searchBtn.addEventListener ('click' , handleClick);
